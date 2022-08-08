49ebe8e439bacd7e8d38f4bca044dde89b06823b
Ryan Malonzo <ryan.malonzo@outlook.com>
2022-08-08 19:12:38 +0200

    Save URL locally

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 
 import os
 
+LOCAL_URL_PATH = "url.txt"
 OUTPUT_LOG_PATH = f"{os.environ['USERPROFILE']}/AppData/LocalLow/miHoYo/Genshin Impact/output_log.txt"
 WISH_HISTORY_URL_PREFIX = "OnGetWebViewPageFinish:"
 
@@ -11,12 +12,32 @@ def fetch_wish_history_url() -> str:
     Fetch and return the URL of the wish history webpage that displays in Genshin Impact.
     This webpage needs to be open in-game when running this program.
     """
+
+    # Try to fetch the URL locally first
+
     try:
-        with open(OUTPUT_LOG_PATH) as output_log:
-            lines = [line for line in output_log.readlines() if line.startswith(WISH_HISTORY_URL_PREFIX)]
-            # It should only match one
-            return lines[0][len(WISH_HISTORY_URL_PREFIX):]
+        with open(LOCAL_URL_PATH) as file:
+            print("Local URL found.")
+            return file.readline()
 
     except FileNotFoundError:
-        print("Wish History URL not found. Make sure it is open in-game before running this program.")
-        raise
+        print("No local URL found. Fetching it from the game's output log...")
+
+        # Fetch it from the game's output log
+
+        try:
+            with open(OUTPUT_LOG_PATH) as output_log:
+                lines = [line for line in output_log.readlines() if line.startswith(WISH_HISTORY_URL_PREFIX)]
+
+                # It should only match one
+                url = lines[0][len(WISH_HISTORY_URL_PREFIX):]
+
+                # Save it locally to speed up next run
+                with open(LOCAL_URL_PATH, "w") as file:
+                    file.write(url)
+
+                return url
+
+        except FileNotFoundError:
+            print("Wish History URL not found. Make sure it is open in-game before running this program.")
+            raise
