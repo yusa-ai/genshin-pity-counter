187605ba40d0a246c710b25cf70c8b068f26c257
Ryan Malonzo <ryan.malonzo@outlook.com>
2022-08-28 19:00:11 +0200

    Get Wish History URL

---
+++ three.py
@@ -0,0 +1,39 @@
+import os
+import re
+import requests
+import subprocess
+import sys
+import tempfile
+from urllib import parse
+from urllib.parse import urlencode
+
+OUTPUT_LOG_FILE_PATH = f"{os.environ['USERPROFILE']}/AppData/LocalLow/miHoYo/Genshin Impact/output_log.txt"
+DATA_FILE_NAME = "data_2"
+
+# Fetch game directory from output_log.txt
+
+with open(OUTPUT_LOG_FILE_PATH) as file:
+    warmup_message = next((line.split("Warmup file")[1] for line in file.readlines()
+                           if line.startswith("Warmup file")), None)
+
+game_dir = warmup_message.split("/Genshin Impact game/")[0].strip()
+
+# Initialize paths based on game directory
+
+data_dir = f"{game_dir}/Genshin Impact game/GenshinImpact_Data/webCaches/Cache/Cache_Data"
+data_file_path = f"{data_dir}/{DATA_FILE_NAME}"
+temp_file_path = f"{tempfile.gettempdir()}/{DATA_FILE_NAME}"
+
+# Copy data file to %temp% using PowerShell
+# A standard copy will not work on Windows while Genshin Impact is running because the file is locked
+
+cmd = f"Copy-Item \"{data_file_path}\" -Destination \"{temp_file_path}\""
+subprocess.Popen(["powershell.exe", cmd], shell=True, stdout=sys.stdout)
+
+# Use regex to find Wish History URL
+
+with open(temp_file_path, errors="ignore") as file:
+    contents = file.read()
+
+wish_history_url = re.findall(r"(https://hk4e-api-os.hoyoverse.com\S+&end_id=0)", contents)[-1]
+print(wish_history_url)
